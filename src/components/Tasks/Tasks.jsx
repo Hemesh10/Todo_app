b9107c932d07f98aa32caac61f73704211918628
Hemesh10 <hemesht97@gmail.com>
2023-10-01 00:02:31 +0530

    added a footer text (kind of!)

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -5,54 +5,64 @@ const Tasks = ({ tasks, deleteHandler, updateTaskById, onComplete }) => {
   const tasksQuantity = tasks.length;
   const completedTasks = tasks.filter((task) => task.isCompleted).length;
   return (
-    <section className="w-[94%] sm:w-full max-w-[736px] py-4 mt-[100px] sm:mt-[74px] mx-auto">
-      <header className="flex justify-between mb-[24px]">
-        <div className="flex items-center gap-2">
-          <p className="text-blue-400 text-sm font-semibold">Tasks Created</p>
-          <span className="text-xs font-bold px-[10px] py-[3px] rounded-full text-[#d9d9d9] bg-[#333333]">
-            {tasksQuantity}
-          </span>
-        </div>
-        <div className="flex items-center gap-2">
-          <p className="text-blue-400 text-sm font-semibold">Completed Tasks</p>
-          <span className="text-xs font-bold px-[10px] py-[3px] rounded-full text-[#d9d9d9] bg-[#333333]">
-            {completedTasks} of {tasksQuantity}
-          </span>
-        </div>
-      </header>
-      {tasks.length > 0 ? (
-        <div className="list">
-          <ScrollArea
-            type="always"
-            scrollbars="vertical"
-            style={{ height: 350 }}
-          >
-            <Box
-              p="2"
-              pr="4"
-              style={{ display: "flex", flexDirection: "column", gap: 12 }}
+    <>
+      <section className="w-[94%] sm:w-full max-w-[736px] py-4 mt-[100px] sm:mt-[74px] mx-auto">
+        <header className="flex justify-between mb-[24px]">
+          <div className="flex items-center gap-2">
+            <p className="text-blue-400 text-sm font-semibold">Tasks Created</p>
+            <span className="text-xs font-bold px-[10px] py-[3px] rounded-full text-[#d9d9d9] bg-[#333333]">
+              {tasksQuantity}
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <p className="text-blue-400 text-sm font-semibold">
+              Completed Tasks
+            </p>
+            <span className="text-xs font-bold px-[10px] py-[3px] rounded-full text-[#d9d9d9] bg-[#333333]">
+              {completedTasks} of {tasksQuantity}
+            </span>
+          </div>
+        </header>
+        {tasks.length > 0 ? (
+          <div className="list">
+            <ScrollArea
+              type="always"
+              scrollbars="vertical"
+              style={{ height: 350 }}
             >
-              {tasks.map((task) => {
-                return (
-                  <Task
-                    key={task.id}
-                    tasks={tasks}
-                    task={task}
-                    deleteHandler={deleteHandler}
-                    updateHandler={updateTaskById}
-                    onComplete={onComplete}
-                  />
-                );
-              })}
-            </Box>
-          </ScrollArea>
-        </div>
-      ) : (
-        <p className="text-lg text-white text-center w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          Nothing to show here 💩
-        </p>
-      )}
-    </section>
+              <Box
+                p="2"
+                pr="4"
+                style={{ display: "flex", flexDirection: "column", gap: 12 }}
+              >
+                {tasks.map((task) => {
+                  return (
+                    <Task
+                      key={task.id}
+                      tasks={tasks}
+                      task={task}
+                      deleteHandler={deleteHandler}
+                      updateHandler={updateTaskById}
+                      onComplete={onComplete}
+                    />
+                  );
+                })}
+              </Box>
+            </ScrollArea>
+          </div>
+        ) : (
+          <p className="text-lg text-white text-center w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            Nothing to show here 💩
+          </p>
+        )}
+      </section>
+      <p className="text-sm text-white absolute bottom-0 right-0 p-2">
+        Made with ❤️ by{" "}
+        <span className="font-semibold bg-gradient-to-r bg-clip-text text-transparent from-blue-500 to-violet-400">
+          Hemesh Thakur
+        </span>
+      </p>
+    </>
   );
 };
 
